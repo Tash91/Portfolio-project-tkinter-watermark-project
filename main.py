f4bdacb3aba21d85214da6abd5db8b50584daa37
Natasha Bryant <97158331+Tash91@users.noreply.github.com>
2025-01-23 14:45:02 +0000

    adding some code skeleton

--- main.py
+++ main.py
@@ -1,6 +1,32 @@
-import tkinter as tk
-from tkinter import PhotoImage
-#Creating main window
+from tkinter import *
+from tkinter import ttk
 
-parent = tk.Tk()
-parent.title("Add a watermark to your image")
+root = Tk()
+root.title("Add watermark to image")
+
+def upload_picture():
+    pass
+
+def add_watermark():
+    pass
+
+def save_new_image():
+    pass
+
+#create a frame widget 
+frame = ttk.Frame(root, padding="5,5,5,5", borderwidth=5, relief="raised")
+
+#create labels
+
+upload_image_label = ttk.Label(root, text='Click here to upload image').grid(column=1, row=2)
+upload_watermark_label = ttk.Label(root, text='Click here to upload watermark').grid(column=3, row=2)
+save_label = ttk.Label(root, text='Click here to save').grid(column=2, row=3)
+
+#create buttons
+
+button = ttk.Button(root, text='Okay', command=upload_picture)
+button = ttk.Button(root, text='Okay', command=add_watermark)
+button = ttk.Button(root, text='Okay', command=save_new_image)
+
+#allows screen to show
+root.mainloop()

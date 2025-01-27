85e9b9fb5d1a581f36ec0d7410b2d9df428cc73d
Natasha Bryant <97158331+Tash91@users.noreply.github.com>
2025-01-27 10:40:19 +0000

    fix delete function to show warning and have a timer

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from PIL import ImageTk, Image
 root = Tk()
 root.title("Add watermark to image")
 
-root.geometry("500x500")
+root.geometry("650x500")
 
 #create canvas
 
@@ -23,25 +23,31 @@ insert_picture=Label(root,image= img)
 
 picture_with_watermark= Label(root,image= img, text='This picture is water marked', compound='center', fg="grey")
 
+delete_warning = Label(root, text='Window will close in 4 seconds', font=("Courier", 15))
+
+
 def upload_picture():
     insert_picture.grid(column=1, row=1)
     insert_picture.image = img
 
    
 def add_watermark():
+    insert_picture.after(1000, insert_picture.destroy())
     picture_with_watermark.grid(column=1, row=1)
     picture_with_watermark.image = img
 
 def delete_image():
+    delete_warning.grid(column=1, row=0)
     picture_with_watermark.after(1000, picture_with_watermark.destroy())
+    root.after(4000,lambda:root.destroy())
 
 
 
 #creating buttons
 
-button = ttk.Button(root, text='Upload picture', command=upload_picture).grid(column=0, row=1)
-button = ttk.Button(root, text='Add watermark', command=add_watermark).grid(column=0, row=2)
-button = ttk.Button(root, text='Delete picture', command=delete_image).grid(column=0, row=3)
+button = ttk.Button(root, text='Upload picture', command=upload_picture).grid(column=0, row=2)
+button = ttk.Button(root, text='Add watermark', command=add_watermark).grid(column=1, row=2)
+button = ttk.Button(root, text='Delete picture', command=delete_image).grid(column=2, row=2)
 
 #allows screen to show
 root.mainloop()
